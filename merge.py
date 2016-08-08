5fd9f2d5fd0752c2622dfa7fb27d5cd978537fe9
Peter Tang <petertangisme@gmail.com>
2016-08-07 21:42:28 -0700

    [Peter] Stop hardcoding passwords. should have done this first tbh

--- merge.py
+++ merge.py
@@ -1,4 +1,4 @@
-import os
+import os, getpass
 import PyPDF2
 
 ext = ".pdf"
@@ -8,39 +8,47 @@ output_path = 'output/'
 
 
 def main():
-    make_pdfs()
+    passwords = get_passwords()
+    make_pdfs(passwords)
 
 
-def make_pdfs():
+def get_passwords():
+    passwords = [raw_input("Please enter customer password:\n"), getpass.getpass("Please enter master password:\n")]
+    print "If you typed in the wrong password, please press ctrl+c to quit."
+    return passwords
+
+
+def make_pdfs(passwords):
     with open(orig_path + 'CopyrightStatements.pdf', 'rb') as cp_statements:
-        # loop through years, types
+        # get input for this pls
         years = xrange(2003, 2014)
-        [create_pdf(year, cp_statements) for year in years]
+        [create_pdf(year, cp_statements, passwords) for year in years]
 
 
-def create_pdf(year, cp_statements):
+def create_pdf(year, cp_statements, passwords):
     try:
         with open(orig_path + 'USABO {:d} Open Exam.pdf'.format(year), 'rb') as usabo_f:
             output_name = "USABO {:d} Open".format(year)
             temp_fname = orig_path + output_name + temp + ext
+            
             with open(temp_fname, "w+b") as output_pdf:
                 merge_files(cp_statements, output_pdf, usabo_f)
 
-                output_name = method_name(output_name, output_pdf)
+                output_name = encrypt_file(output_name, output_pdf, passwords)
 
             os.remove(temp_fname)
-            print "Finished output for file {:s}".format(output_name)
+            print "Finished output for file `{:s}`".format(output_name)
 
     except IOError:
         print 'Unable to open {:d} pdf'.format(year)
 
 
-def method_name(output_name, output_pdf):
+def encrypt_file(output_name, output_pdf, passwords):
     output_in = PyPDF2.PdfFileReader(output_pdf)
     output_encrypted = PyPDF2.PdfFileWriter()
 
     output_encrypted.appendPagesFromReader(output_in)
-    output_encrypted.encrypt(user_pwd="email", owner_pwd="***REMOVED***")
+    output_encrypted.encrypt(user_pwd=passwords[0], owner_pwd=passwords[1])
 
     output_name = output_path + output_name + ext
 
@@ -57,4 +65,8 @@ def merge_files(cp_statements, output_pdf, usabo_f):
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print "Goodbye! Be sure to clean up any temp files!"
+    print "Thank you! Have a nice day!"
